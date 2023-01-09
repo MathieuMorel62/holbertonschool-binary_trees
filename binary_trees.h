67dab5083e0eeeb0e2118fb80cef429bf55e2473
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-09 10:57:42 +0100

    add data structure

--- binary_trees.h
+++ binary_trees.h
@@ -28,15 +28,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-
-/* Binary Search Tree */
-typedef struct binary_tree_s bst_t;
-
-/* AVL Tree */
-typedef struct binary_tree_s avl_t;
-
-/* Max Binary Heap */
-typedef struct binary_tree_s heap_t;
+typedef struct binary_tree_s bst_t; /* Binary Search Tree */
+typedef struct binary_tree_s avl_t; /* AVL Tree */
+typedef struct binary_tree_s heap_t; /* Max Binary Heap */
 
 /******************* PROTOTYPES *******************/
 
