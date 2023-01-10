cc3cce9ec2f3e480be4f985563b476b35401f012
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-10 15:58:06 +0100

    betty coding style

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,7 +13,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	
+
 	if (tree->left && tree->right)
 		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
