8904e58663e1c108b98bf329e71cd572c15d70bc
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-10 09:53:01 +0100

    Update 11-binary_tree_size.c
    Betty coding style

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,7 +9,7 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
     if (tree == NULL)
-        return 0;
+        return (0);
     else
         return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 }
