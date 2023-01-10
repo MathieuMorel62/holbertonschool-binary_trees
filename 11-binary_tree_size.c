1865dd66a19043d97078eb04d683a1f396abcfc3
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-10 10:20:02 +0100

    betty coding style

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of a binary tree
- * @tree: a pointer to the root node of the tree to measure the size
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the size
  * Return: 0 if tree is NULL
  */
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree==NULL)
-        return 0;
-    else
-        return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+	if (tree == NULL)
+		return (0);
+	else
+		return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 }
