a1665d50ff998628aba9b89e9fb4de60fc40e1ac
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-01-10 18:08:19 +0100

    betty coding style

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -59,10 +59,14 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (binary_tree_balance(tree) == 0)
 	{
-		if ((binary_tree_height(tree->left) == 0) && (binary_tree_height(tree->right) == 0))
+		if (binary_tree_height(tree->left) == 0)
 			return (1);
 
-		return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+		if (binary_tree_height(tree->right) == 0)
+			return (1);
+
+		return (binary_tree_is_perfect(tree->left));
+		return (binary_tree_is_perfect(tree->right));
 	}
 	return (0);
 }
